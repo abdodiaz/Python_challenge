c869f3f51a7395482d52c96ddeae9ca81168b914
Achraf CHAIR <139182135+achrafdevl@users.noreply.github.com>
2025-07-03 10:03:46 +0100

    Update Challenge_1.py

--- Challenge_3/Challenge_1.py
+++ Challenge_3/Challenge_1.py
@@ -1,4 +1,13 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> refs/remotes/origin/Master
+notes = [12, 4, 14, 11, 18, 13, 7, 10, 5, 9, 15, 8, 14, 16]
+
+moyenne = sum(notes) / len(notes)
+
+notes_superieures = []
+
+for note in notes:
+    if note > moyenne:
+        notes_superieures.append(note)
+
+print("Moyenne des notes :", moyenne)
+print("Notes supérieures à la moyenne :", notes_superieures)
